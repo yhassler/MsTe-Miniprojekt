6e0fba5e33b4ddd3158011f96d1a82dc0de1ca49
m2weber <43742536+m2weber@users.noreply.github.com>
2018-12-05 11:10:29 +0100

    Add UI for editing customers
    - Add the page for customers
    - Move pages to their own folder

---
+++ Miniprojekt/AutoReservation.UI/Views/Pages/KundeEditierenPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace AutoReservation.UI
+{
+    /// <summary>
+    /// Interaction logic for KundeEditierenPage.xaml
+    /// </summary>
+    public partial class KundeEditierenPage : Page
+    {
+        public KundeEditierenPage()
+        {
+            InitializeComponent();
+        }
+
+        private void UIElement_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+    }
+}
